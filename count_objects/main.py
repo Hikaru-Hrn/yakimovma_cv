4376e23d9c6143ba368eff0ec4a0f6f2cd873383
Hikaru-Hrn <geniysenkou@gmail.com>
2025-03-07 20:35:16 +0800

    Update main.py

--- count_objects/main.py
+++ count_objects/main.py
@@ -28,7 +28,7 @@ def count_objects(image):
 
 
 #Загружаем и обрабатываем
-image = np.load('ex6npy.txt')
+image = np.load('example1.txt')
 if image.ndim == 3:
     image = image[:, :, 0] #Возьмем первый канал, если файл многоканальный
 if not np.all(np.isin(image, [0, 1])):
